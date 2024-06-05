a18284d16e57e9a2db334dbadb9f49e49254c7dc
Hari Krishna Anem <anemharikrishna@gmail.com>
2024-06-05 17:53:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -43,35 +43,44 @@ function App() {
   };
 
   return (
-    <Container className="my-4">
+    <Container fluid className="my-4">
       <nav className="navbar navbar-light bg-light mb-5">
-        <div className="container-fluid d-flex justify-content-center">
+        <div className="container d-flex justify-content-center">
           <span className="navbar-brand mb-0 h1 text-center">
-            Web scraper - Vite + React + TS + Bootstrap CSS + Cheerio (In nodejs)
+            Web scraper - Vite + React + TS + Bootstrap CSS + Cheerio (In
+            nodejs)
           </span>
         </div>
       </nav>
       <div className="d-flex justify-content-center mb-4">
-        <form onSubmit={handleSubmit} className="d-flex w-50">
+        <form
+          onSubmit={handleSubmit}
+          className="d-flex flex-column flex-sm-row w-100 w-sm-50"
+        >
           <input
             type="text"
-            className="form-control mr-2"
+            className="form-control mr-2 mb-2 mb-sm-0"
             placeholder="Enter your mobile name or model"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
             required
           />
-          <button type="submit" className="btn btn-primary me-1">
-            Submit
-          </button>
-          <button type="button" className="btn btn-secondary" onClick={handleClear}>
-          Clear
-        </button>
+          <div className="d-flex">
+            <button type="submit" className="btn btn-primary me-1">
+              Submit
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={handleClear}
+            >
+              Clear
+            </button>
+          </div>
         </form>
-        
       </div>
       {noData && (
-        <div className="alert alert-danger w-25" role="alert" style={{ marginLeft: "350px" }}>
+        <div className="alert alert-danger w-100 w-sm-25 mx-auto" role="alert">
           {noData}
         </div>
       )}
@@ -85,11 +94,9 @@ function App() {
       {specifications &&
         specifications.general &&
         specifications.general.name && (
-          <MobileSpecification
-            specifications={specifications}
-          />
+          <MobileSpecification specifications={specifications} />
         )}
-        <FloatingIcon />
+      <FloatingIcon />
     </Container>
   );
 }
